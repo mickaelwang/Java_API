f38be870a9ed689579c1c7fcbf3c424a7b793e07
ml <mlorente@antidot.net>
2013-12-13 09:41:03 +0100

    Update documentation for afs:store filter

--- doc/html/search/pages_61.js
+++ doc/html/search/pages_61.js
@@ -4,6 +4,5 @@ var searchData=
   ['afs_20search_20query_20coder_2fdecoder_20api',['AFS search query coder/decoder API',['../afs_search_coder.html',1,'']]],
   ['afs_20search_20query_20api',['AFS search query API',['../afs_search_query.html',1,'']]],
   ['afs_20search_20reply_20api',['AFS search reply API',['../afs_search_reply.html',1,'']]],
-  ['antidot_20back_20office_20php_20api_20documentation',['Antidot Back Office PHP API Documentation',['../antidot_back_office.html',1,'']]],
   ['antidot_20java_20api_20documentation',['Antidot JAVA API documentation',['../index.html',1,'']]]
 ];
